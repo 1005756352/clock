a9a166338440c67e9d113d3416d4553860a34478
fengjin <fengjin.qq.com@qq.com>
2016-07-26 17:22:47 +0800

    xuanli

--- html/countdown.js
+++ html/countdown.js
@@ -1,10 +1,12 @@
 var WINDOW_WIDTH = 1024;
-var WINDOW_HEIGHT = 768;
+var WINDOW_HEIGHT = 668;
 var RADIUS = 8;
 var MARGIN_LEFT = 20;
 var MARGIN_TOP = 60;
-const endTime = new Date(2016,6,23,20,47,52);
+const endTime = new Date(2016,6,26,20,47,52);
 var curShowTimeSeconds = 0;
+var balls = [];
+var colors = ["#33b5e5","#0099cc","#aa66cc","9933cc","99cc00","#669900","#ffbb33","#ff8800","#ff4444","#cc0000"]
 window.onload = function(){
 				var canvas = document.getElementById("canvas");
 				canvas.height = WINDOW_HEIGHT;
@@ -30,9 +32,42 @@ function update(){
 	var curSeconds = curShowTimeSeconds%60;
 	
 	if(nextSeconds != curSeconds){
+		if(parseInt(curHours/10) != parseInt(nextHours/10)){
+			addBalls(MARGIN_LEFT+0,MARGIN_TOP,parseInt(curHours/10));
+		}
+		if(parseInt(curHours%10) != parseInt(nextHours%10)){
+			addBalls(MARGIN_LEFT+15*(RADIUS+1),MARGIN_TOP,parseInt(curHours%10));
+		}
+		if(parseInt(curMinutes/10) != parseInt(nextMinutes/10)){
+			addBalls(MARGIN_LEFT+39*(RADIUS+1),MARGIN_TOP,parseInt(curMinutes/10));
+		}
+		if(parseInt(curMinutes%10) != parseInt(nextMinutes%10)){
+			addBalls(MARGIN_LEFT+54*(RADIUS+1),MARGIN_TOP,parseInt(curMinutes%10));
+		}
+		if(parseInt(curSeconds/10) != parseInt(nextSeconds/10)){
+			addBalls(MARGIN_LEFT+78*(RADIUS+1),MARGIN_TOP,parseInt(curSeconds/10));
+		}
+		if(parseInt(curSeconds%10) != parseInt(nextSeconds%10)){
+			addBalls(MARGIN_LEFT+93*(RADIUS+1),MARGIN_TOP,parseInt(curSeconds%10));
+		}
 		curShowTimeSeconds = nextShowTimeSeconds;
 	}
+	updateBalls();
 }
+
+function updateBalls(){
+	for(var i=0;i<balls.length;i++){
+		balls[i].x = balls[i].x+balls[i].vx;
+		balls[i].y = balls[i].y+balls[i].vy;
+		balls[i].vy = balls[i].vy+balls[i].g;
+		
+		if(balls[i].y >= WINDOW_HEIGHT-RADIUS){
+			balls[i].y = WINDOW_HEIGHT-RADIUS;
+			balls[i].vy = -balls[i].vy*0.75;
+		}
+	}
+}
+
 function getCurrentShowTimeSeconds () {
 	var curTime = new Date();
 	var ret = endTime.getTime() - curTime.getTime();
@@ -53,7 +88,14 @@ function render(cxt){
 	renderDigit(MARGIN_LEFT+69*(RADIUS+1),MARGIN_TOP,10,cxt);
 	renderDigit(MARGIN_LEFT+78*(RADIUS+1),MARGIN_TOP,parseInt(seconds/10),cxt);
 	renderDigit(MARGIN_LEFT+93*(RADIUS+1),MARGIN_TOP,parseInt(seconds%10),cxt);
-
+	
+	for(var i=0;i<balls.length;i++){
+		cxt.fillStyle = balls[i].color;
+		cxt.beginPath();
+		cxt.arc(balls[i].x,balls[i].y,RADIUS,0,2*Math.PI.true);
+		cxt.closePath();
+		cxt.fill();
+	}
 
 }
 
@@ -66,6 +108,24 @@ function renderDigit(x,y,num,cxt){
 				cxt.arc(x+2*(RADIUS+1)*j+(RADIUS+1),y+2*(RADIUS+1)*i+(RADIUS+1),RADIUS,0,2*Math.PI);
 				cxt.closePath();
 				cxt.fill();
+			}
+		}
+	}
+}
+
+function addBalls(x,y,num){
+	for(var i=0;i<digit[num].length;i++){
+		for(var j=0;j<digit[num][i].length;j++){
+			if(digit[num][i][i] == 1){
+				var aBall ={
+					x:x+j*2*(RADIUS+1)+(RADIUS+1),
+					y:y+i*2*(RADIUS+1)+(RADIUS+1),
+					g:1.5+Math.random(),
+					vx:Math.pow(-1,Math.ceil(Math.random()*1000))*4,
+					vy:-5,
+					color:colors[Math.floor(Math.random()*colors.length)]
+				}
+				balls.push(aBall);
 			}
 		}
 	}
